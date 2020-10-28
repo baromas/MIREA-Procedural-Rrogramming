882f20f1ea252082f9de894eed9663b9b064043d
Balandin Roman <baromas007@gmail.com>
2020-10-28 14:30:52 +0500

    Add files via upload

---
+++ ДЗ №1/Domashka2/Domashka2/main.cpp
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <iostream>;
+using namespace std;
+int main(){
+	int a;
+	int b;
+	cin >> a;
+	cin >> b;
+
+	cout << a - b << "\n";
+	cout << a + b << "\n";
+	cout << a * b << endl;
+	cout << a / b;
+}

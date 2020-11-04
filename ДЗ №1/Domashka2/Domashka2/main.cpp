f963010e593ca6b7226cd905291dcfbac16ca267
KennyDos <kenydiii@mail.ru>
2020-11-04 18:47:41 +0300

    1.2

--- ДЗ №1/Domashka2/Domashka2/main.cpp
+++ ДЗ №1/Domashka2/Domashka2/main.cpp
@@ -4,11 +4,17 @@ using namespace std;
 int main(){
 	int a;
 	int b;
+	cout << "Enter a" << "\n";
 	cin >> a;
+	cout << "Enter b" "\n";
 	cin >> b;
 
-	cout << a - b << "\n";
-	cout << a + b << "\n";
-	cout << a * b << endl;
-	cout << a / b;
-}
+	cout << "Differrence: " << a - b << "\n";
+	cout << "Addition: " << a + b << "\n";
+	cout << "Multiplication: " << a * b << endl;
+	if (b == 0) {
+		cout << "Impossible to calculate division (b = 0)";
+	}
+	else {
+		cout << "Divivsion: " << a / b;
+	}}
